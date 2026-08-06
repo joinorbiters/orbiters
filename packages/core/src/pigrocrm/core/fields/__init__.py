873dcf70f62a50083166761195335323f5a25dbb
Ivan Sala <slavni96@gmail.com>
2026-08-06 20:06:44 +0900

    feat: pure custom-field validator with per-type coercion and actionable errors

---
+++ packages/core/src/pigrocrm/core/fields/__init__.py
@@ -0,0 +1,10 @@
+from pigrocrm.core.fields.types import FIELD_TYPES, FieldSpec, FieldType
+from pigrocrm.core.fields.validator import coerce_value, validate_custom_fields
+
+__all__ = [
+    "FIELD_TYPES",
+    "FieldSpec",
+    "FieldType",
+    "coerce_value",
+    "validate_custom_fields",
+]
